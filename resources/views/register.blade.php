d3972378975d6eb5c46c9dee3877dc20f81e080e
Ozodbek <email@misol.com>
2025-04-02 14:29:39 +0500

    fixed problem in register and added delete button to index

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -20,10 +20,19 @@
             </div>
             <div class="mb-3">
                 <label for="confirm-password" class="form-label">Confirm Password</label>
-                <input type="password" class="form-control" id="confirm-password" name="confirmPassword">
+                <input type="password" class="form-control" id="confirm-password" name="password_confirmation">
             </div>
             <button type="submit" class="btn btn-primary">Register</button>
         </form>
     </div>
-    
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
     @endsection
